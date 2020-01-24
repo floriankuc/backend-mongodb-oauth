4f88b3d05f27289bcdbdf076ca5dc8b48550a417
Florian Kuc <florian.kuc@gmail.com>
2020-01-24 21:39:04 +0100

    WIP

--- config/dev.js
+++ config/dev.js
@@ -1,14 +1,13 @@
 //FÜR DEV KEYS
 
 module.exports = {
-    googleClientID: '53041128289-5mc7fs3b0n6pfjtcb3hhpn2ro2sd13c0.apps.googleusercontent.com',
-    googleClientSecret: 'inzHVi5FCSUUXuNWg6K6YsZC',
-    mongoURI: 'mongodb+srv://florianadmin:LlKfm8dkCNxmH7N3@backend2-bfquv.mongodb.net/test?retryWrites=true&w=majority',
-    cookieKey: 'uiasdfkjsadfiuodasfjiaskljasdfjklsadfjkl'
-}
-
-
-
+  googleClientID:
+    "53041128289-5mc7fs3b0n6pfjtcb3hhpn2ro2sd13c0.apps.googleusercontent.com",
+  googleClientSecret: "inzHVi5FCSUUXuNWg6K6YsZC",
+  mongoURI:
+    "mongodb+srv://florianadmin:LlKfm8dkCNxmH7N3@backend2-bfquv.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "uiasdfkjsadfiuodasfjiaskljasdfjklsadfjkl"
+};
 
 // florianadmin - LlKfm8dkCNxmH7N3 pw
 
--- services/passport.js
+++ services/passport.js
@@ -1,36 +1,40 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
-    done(null, user.id)
+  done(null, user.id);
 });
 passport.deserializeUser((id, done) => {
-    User.findById(id)
-        .then(user => {
-            done(null, user);
-        })
-})
-
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 //ROUTE HANDLER
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback', //HANDLING AUTH ROUTE REDIRECT
-    proxy: true
-}, async (accessToken, refreshToken, profile, done) => { //CREATE NEW USER WITH USER SCHEMA
-    const existingUser = await User.findOne({
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback", //HANDLING AUTH ROUTE REDIRECT
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      //CREATE NEW USER WITH USER SCHEMA
+      const existingUser = await User.findOne({
         googleId: profile.id
-    })
+      });
 
-    if (existingUser) {
+      if (existingUser) {
         //THERE IS AN EXISTING USER
         return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save(); //save() saved USER TO MONGODB
+      done(null, user);
     }
-    const user = await new User({ googleId: profile.id }).save() //save() saved USER TO MONGODB
-    done(null, user)
-}));
+  )
+);
